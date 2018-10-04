08a45c9d412b447f4dc43a8d758c910e5706dc88
adel <adel.raheli@gmail.com>
2018-10-05 00:36:33 +0330

    config change and fixed issue

--- src/Config/LAM.php
+++ src/Config/LAM.php
@@ -6,11 +6,11 @@ return [
 
     // ======================================================================
     // never remove 'web', . just put your middleware like auth or admin (if you have) here. eg: ['web','auth']
-    'private_middlewares'  => explode(',', env('BACKEND_LUM_MIDDLEWARES', 'web')),
-    'public_middlewares'   => explode(',', env('BACKEND_LUM_MIDDLEWARES', 'web')),
+    'private_middlewares'  => explode(',', env('LAM_BACKEND_MIDDLEWARES', 'web')),
+    'public_middlewares'   => explode(',', env('LAM_BACKEND_MIDDLEWARES', 'web')),
     // you can change default route from sms-admin to anything you want
-    'private_route_prefix' => env('BACKEND_LAM_ROUTE_PREFIX', 'LUM'),
-    'public_route_prefix'  => env('FRONTEND_LAM_ROUTE_PREFIX', 'LUM'),
+    'private_route_prefix' => env('LAM_BACKEND_ROUTE_PREFIX', 'LUM'),
+    'public_route_prefix'  => env('LAM_FRONTEND_ROUTE_PREFIX', 'LUM'),
     // ======================================================================
 
 ];
